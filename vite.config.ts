af2c26f373123ca0a526507fca67e40795e44e07
Kirsten Taylor <kirsten@shufflestudio.io>
2026-09-26 11:31:24 -0400

    Connect Cloudflare D1 database

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import hostingConfig from "./.openai/hosting.json";
 import { sites } from "./build/sites-vite-plugin";
 
 const SITE_CREATOR_PLACEHOLDER_DATABASE_ID =
-  "00000000-0000-4000-8000-000000000000";
+  "c355dbba-aaa7-4983-90ad-b9da9e003ffb";
 
 const { d1, r2 } = hostingConfig;
 
